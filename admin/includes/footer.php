d9a7a3cd1b6aa54d0a29ac0f85a05273e4443372
pavankumar431 <silentboypa1@gmail.com>
2025-01-03 19:07:40 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2020 <a href="http://nirajtechtalk.com/">Niraj Techtalk</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
